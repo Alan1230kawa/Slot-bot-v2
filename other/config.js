f6cd8473b78e7be2d59c0c2005b5545e3f7746fb
Alan1230kawa <141342017+Alan1230kawa@users.noreply.github.com>
2024-07-15 12:32:39 +0300

    Update config.js
    Signed-off-by: Alan1230kawa <141342017+Alan1230kawa@users.noreply.github.com>

--- other/config.js
+++ other/config.js
@@ -4,13 +4,13 @@ const cf = {
 
     guild_x: "1224068932621701130", //your guild id
 
-    role_x: "1256264733305213019", //slot user role id
+    role_x: "1262337090822213696", //slot user role id
 
-    staff_x: "None", // staff role id
+    staff_x: "1262336779990859777", // staff role id
 
-    logchannel: '', //channel id for slot revoke notice
+    logchannel: '1262340873656729632', //channel id for slot revoke notice
 
-    slotrule: `set your slot rules`, // your slot rules
+    slotrule: `test`, // your slot rules
   }
 }
 
