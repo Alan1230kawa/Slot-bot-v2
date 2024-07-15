80b1e3490b7299f0c5257d137d5bd470a8609098
Alan1230kawa <141342017+Alan1230kawa@users.noreply.github.com>
2024-07-15 11:40:11 +0300

    Update config.js
    Signed-off-by: Alan1230kawa <141342017+Alan1230kawa@users.noreply.github.com>

--- other/config.js
+++ other/config.js
@@ -2,9 +2,9 @@ const cf = {
   config: {
 
 
-    guild_x: "None", //your guild id
+    guild_x: "1224068932621701130", //your guild id
 
-    role_x: "None", //slot user role id
+    role_x: "1256264733305213019", //slot user role id
 
     staff_x: "None", // staff role id
 
